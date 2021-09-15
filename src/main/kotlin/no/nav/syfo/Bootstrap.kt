f8a923cc070ec979581588c217da1dd8040e4292
andreasnilsen <danduras@gmail.com>
2021-09-15 15:16:37 +0200

    read from kafka and put in the databases

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -1,10 +1,24 @@
 package no.nav.syfo
 
 import io.prometheus.client.hotspot.DefaultExports
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import no.nav.syfo.application.ApplicationServer
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.application.createApplicationEngine
 import no.nav.syfo.application.db.Database
+import no.nav.syfo.kafka.aiven.KafkaUtils
+import no.nav.syfo.kafka.toConsumerConfig
+import no.nav.syfo.narmesteleder.NarmestelederService
+import no.nav.syfo.narmesteleder.db.NarmestelederDb
+import no.nav.syfo.narmesteleder.kafka.model.NarmestelederKafkaMessage
+import no.nav.syfo.util.JacksonKafkaDeserializer
+import no.nav.syfo.util.Unbounded
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -21,7 +35,33 @@ fun main() {
     val applicationServer = ApplicationServer(applicationEngine, applicationState)
     applicationServer.start()
 
+    val kafkaConsumer = KafkaConsumer(
+        KafkaUtils.getAivenKafkaConfig().also { it[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "100" }.toConsumerConfig("narmesteleder-arbeidsforhold", JacksonKafkaDeserializer::class),
+        StringDeserializer(),
+        JacksonKafkaDeserializer(NarmestelederKafkaMessage::class)
+    )
+
+
     val database = Database(env, applicationState)
 
+    val narmestelederService = NarmestelederService(kafkaConsumer, NarmestelederDb(database), applicationState)
+
     applicationState.ready = true
+
+    startBackgroundJob(applicationState) {
+        narmestelederService.start()
+    }
+
+}
+
+fun startBackgroundJob(applicationState: ApplicationState, block: suspend CoroutineScope.() -> Unit) {
+    GlobalScope.launch(Dispatchers.Unbounded) {
+        try {
+            block()
+        } catch (ex: Exception) {
+            log.error("Error in background task, restarting application")
+            applicationState.alive = false
+            applicationState.ready = false
+        }
+    }
 }
--- src/main/kotlin/no/nav/syfo/narmesteleder/db/NarmestelederDb.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/db/NarmestelederDb.kt
@@ -2,19 +2,22 @@ package no.nav.syfo.narmesteleder.db
 
 import no.nav.syfo.application.db.DatabaseInterface
 import no.nav.syfo.narmesteleder.kafka.model.NarmestelederKafkaMessage
+import java.sql.Timestamp
+import java.time.ZoneOffset
 
 class NarmestelederDb(private val database: DatabaseInterface) {
     fun insertOrUpdate(narmesteleder: NarmestelederKafkaMessage) {
         database.connection.use { connection ->
             connection.prepareStatement(
                 """
-               insert into narmesteleder(narmeste_leder_id, orgnummer, bruker_fnr) 
-               values (?, ?, ?) on conflict (narmeste_leder_id) do nothing;
+               insert into narmesteleder(narmeste_leder_id, orgnummer, bruker_fnr, last_update) 
+               values (?, ?, ?, ?) on conflict (narmeste_leder_id) do nothing;
             """
             ).use { preparedStatement ->
                 preparedStatement.setString(1, narmesteleder.narmesteLederId.toString())
                 preparedStatement.setString(2, narmesteleder.orgnummer)
                 preparedStatement.setString(3, narmesteleder.fnr)
+                preparedStatement.setTimestamp(4, Timestamp.from(narmesteleder.aktivFom.atStartOfDay().toInstant(ZoneOffset.UTC)))
                 preparedStatement.executeUpdate()
             }
             connection.commit()
